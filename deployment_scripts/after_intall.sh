d8f6d64abba2e42a063352a598b00ed1ddb0edd6
simples1 <abdullaaden@outlook.com>
2020-02-09 02:14:33 +0000

    appspec

--- deployment_scripts/after_intall.sh
+++ deployment_scripts/after_intall.sh
@@ -3,3 +3,6 @@
 cd /home/deploy/adenio/current
 echo $(pwd) > ppp.rb
 echo $(whoami) > user.rb
+RAILS_ENV=production bundle install --path vendor/bundle
+RAILS_ENV=production bundle exec rake db:migrate
+RAILS_ENV=production bundle exec rake assets:precompile
