7be98e776a0cbf3197c6c5e8869e6c204ad57f92
simples1 <abdullaaden@outlook.com>
2020-02-09 01:25:38 +0000

    add appspec fix

--- deployment_scripts/after_intall.sh
+++ deployment_scripts/after_intall.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 cd /home/deploy/adenio/releases/20200208040839
-RAILS_ENV=production bundle install --path vendor/bundle
-RAILS_ENV=production bundle exec rake db:migrate
-RAILS_ENV=production bundle exec rake assets:precompile
+touch test.rb
