52bde17214bfa93cd1f49e143333e73de0f5c372
simples1 <abdullaaden@outlook.com>
2020-02-10 00:55:41 +0000

    symlink usod

--- deployment_scripts/after_install.sh
+++ deployment_scripts/after_install.sh
@@ -2,13 +2,14 @@
 
 # source /home/deploy/.bashrc
 PATH=/home/deploy/.rbenv/plugins/ruby-build/bin:/home/deploy/.rbenv/shims:/home/deploy/.rbenv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
-cd /home/deploy/adenio/current
-RAILS_ENV=production bundle install --path vendor/bundle
-RAILS_ENV=production bundle exec rake db:migrate
-RAILS_ENV=production bundle exec rake assets:precompile
 
 datetimestamp=$(date +%Y%m%d%H%M%S)
 mv /home/deploy/adenio/releases/adenio/ /home/deploy/adenio/releases/$datetimestamp
 
 cd /home/deploy/adenio
 sudo ln -sfn /home/deploy/adenio/releases/$datetimestamp current
+
+cd /home/deploy/adenio/current
+RAILS_ENV=production bundle install --path vendor/bundle
+RAILS_ENV=production bundle exec rake db:migrate
+RAILS_ENV=production bundle exec rake assets:precompile
