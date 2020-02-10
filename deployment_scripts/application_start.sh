a9bbf4dc1272a2dbb7f6c03223422ae6935ebdea
simples1 <abdullaaden@outlook.com>
2020-02-10 00:32:04 +0000

    symlink usod

--- deployment_scripts/application_start.sh
+++ deployment_scripts/application_start.sh
@@ -2,6 +2,6 @@
 PATH=/home/deploy/.rbenv/plugins/ruby-build/bin:/home/deploy/.rbenv/shims:/home/deploy/.rbenv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
 
 sudo service nginx restart
-cd /home/deploy/adenio/current
 rm /home/deploy/adenio/shared/tmp/sockets/adenio-puma.sock
+cd /home/deploy/adenio/current
 bundle exec puma -C /home/deploy/adenio/shared/puma.rb config.ru -d
