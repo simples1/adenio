4f583e75fa2ba7a5d83cf6088bd3cd57fbb8333d
simples1 <abdullaaden@outlook.com>
2020-02-10 22:02:29 +0000

    version 6

--- deployment_scripts/application_start.sh
+++ deployment_scripts/application_start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 PATH=/home/deploy/.rbenv/plugins/ruby-build/bin:/home/deploy/.rbenv/shims:/home/deploy/.rbenv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
 
-sudo service nginx restart
-
 FILE=/home/deploy/adenio/shared/tmp/sockets/adenio-puma.sock
 if [ -f "$FILE" ]; then
-  rm $FILE
+  rm /home/deploy/adenio/shared/tmp/sockets/*
 fi
 
 cd /home/deploy/adenio/current
 bundle exec puma -C /home/deploy/adenio/shared/puma.rb config.ru -d
+
+sudo service nginx restart
